ae5e8750f7f598a9e88d86b65d89f211a68c1f76
tej351 <tejashreesonwane4@gmai.com>
2026-09-23 13:00:02 +0530

    Added Frontend UI

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 
+# Load trained model
 model = joblib.load("model/model.pkl")
 
+# Home page
 @app.route("/")
 def home():
-    return "Student Performance Predictor API"
+    return render_template("index.html")
 
+# Prediction route
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    data = request.json
+    study_hours = float(request.form["StudyHours"])
+    attendance = float(request.form["Attendance"])
+    previous_marks = float(request.form["PreviousMarks"])
+    assignments = float(request.form["Assignments"])
 
     features = np.array([[
-        data["StudyHours"],
-        data["Attendance"],
-        data["PreviousMarks"],
-        data["Assignments"]
+        study_hours,
+        attendance,
+        previous_marks,
+        assignments
     ]])
 
-    prediction = model.predict(features)
+    prediction = model.predict(features)[0]
 
-    return jsonify({
-        "Predicted Marks": round(prediction[0], 2)
-    })
+    return render_template(
+        "index.html",
+        prediction=round(prediction, 2)
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
